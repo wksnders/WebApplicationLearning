131bf01853084669cb74c1f00d7915ee277ddb09
wksnders <42388072+wksnders@users.noreply.github.com>
2023-12-23 19:04:32 -0700

    tutorial for making model

---
+++ BlazorApp2/BlazorApp2/Models/ListItem.cs
@@ -0,0 +1,11 @@
+﻿namespace BlazorApp2.Models
+{
+    public class ListItem
+    {
+        public int Id { get; set; }
+        public required string Name { get; set; }
+        public required string Genere { get; set; }
+        public decimal Price { get; set; }
+        public DateTime ReleaseDate { get; set; }
+    }
+}
