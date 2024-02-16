a5e18a0ad24a1c229d327539f94bd946fbf9f10b
wksnders <42388072+wksnders@users.noreply.github.com>
2024-02-16 12:32:02 -0700

    full refactor of counter game

---
+++ BlazorApp2/BlazorApp2/Models/CounterGameModel.cs
@@ -0,0 +1,160 @@
+﻿namespace BlazorApp2.Models
+{
+    public struct ActionWithCost
+    {
+        public double cost;
+        public Action action;
+    }
+
+    public struct CounterItem {
+        public double autoClickerValue;
+        public string IconPath;
+    }
+    public class CounterGameModel
+    {
+        public static CounterGameModel Instance {
+            get {
+                if (inst is null) {
+                    inst = new CounterGameModel();
+                }
+                return inst;
+            }
+        }
+        private static CounterGameModel? inst;
+
+
+        double gameTickSpeed = 100000; //time between frames in microseconds
+
+        bool gameIsRunning;
+        public int DisplayCount
+        {
+            get { return (int)(Math.Floor(currentCount)); }
+            private set { currentCount = (double)value; }
+        }
+        public double currentCount = 0;
+        public double PlayerClickValue { get; set; } = 1;
+        public double autoIncrementAmount { get; set; } = 0;
+        PeriodicTimer gameLoopTimer;
+
+        Queue<ActionWithCost> playerInputs = new Queue<ActionWithCost>();
+        public List<Action> lateUpdateActions = new List<Action>();
+
+        public CounterGameModel()
+        {
+            gameLoopTimer = new PeriodicTimer(TimeSpan.FromMicroseconds(gameTickSpeed));
+        }
+
+        public void StartGame()
+        {
+            gameIsRunning = true;
+            _ = GameLoopAsync(gameLoopTimer);
+        }
+
+        public void PauseUnpause() {
+            gameIsRunning = !gameIsRunning;
+        }
+
+        public void PlayerClicked()
+        {
+            currentCount += PlayerClickValue;
+        }
+
+        public void PlayerGotItem(CounterItemType type) { 
+            
+        }
+
+        public void EnqueuePlayerAction(ActionWithCost action) {
+            playerInputs.Enqueue(action);
+        }
+
+        private bool TryMakePurchaseWithCost(double Cost)
+        {
+            Cost = Math.Floor(Cost);
+            if (currentCount < Cost)
+            {
+                return false;
+            }
+            currentCount -= Cost;
+            return true;
+        }
+
+        private async Task GameLoopAsync(PeriodicTimer timer)
+        {
+            while (gameIsRunning)
+            {
+                await timer.WaitForNextTickAsync();
+                Update();
+                LateUpdate();
+            }
+        }
+
+        private void Update()
+        {
+            while (playerInputs.Count > 0)
+            {
+                //dequeue player actions and apply effects of those actions
+                ActionWithCost actionWithCost = playerInputs.Dequeue();
+                if (TryMakePurchaseWithCost(actionWithCost.cost))
+                {
+                    actionWithCost.action?.Invoke();
+                }
+            }
+
+
+            currentCount += autoIncrementAmount;
+
+        }
+
+        private void LateUpdate()
+        {
+            foreach (Action action in lateUpdateActions) { 
+                action?.Invoke();
+            }
+        }
+
+
+    }
+    public struct CounterShopItem
+    {
+        public string Name;
+        public bool IsLocked;
+        public double Cost;
+        public double CostIncreaseFactor;
+    }
+
+
+    public enum CounterItemType{ 
+        None,
+        IncreasePlayerClickValue,
+        AutoClicker
+        
+    }
+    public class CounterGameShopModel
+    {
+        public Dictionary<CounterItemType, CounterShopItem> shopItems { get; private set; }
+        public CounterGameShopModel(Dictionary<CounterItemType, CounterShopItem> initialShopItems) {
+            shopItems = initialShopItems;
+        }
+
+        public bool TryBuyShopItem(Action<CounterItemType> playerGotItem,CounterItemType type,out ActionWithCost action) 
+        {
+            CounterShopItem item;
+            if (!shopItems.TryGetValue(type,out item) || item.IsLocked) {
+                action = new ActionWithCost{ };
+                return false;
+            }
+            
+            item.IsLocked = true;
+            action = new ActionWithCost
+            {
+                cost = item.Cost,
+                action = () => {
+                    playerGotItem(type);
+                    item.Cost = item.Cost * item.CostIncreaseFactor;
+                    item.IsLocked = false;
+                }
+            };
+            return true;
+        }
+    }
+}
