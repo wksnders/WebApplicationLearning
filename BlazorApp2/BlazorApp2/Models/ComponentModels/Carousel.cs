535b68c5f2e0f7ee61c0fd2ff64a7d6fcdc654f2
wksnders <42388072+wksnders@users.noreply.github.com>
2024-02-25 20:21:01 -0700

    added a reusable Carousel Dot selector

---
+++ BlazorApp2/BlazorApp2/Models/ComponentModels/Carousel.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace BlazorApp2.Models.ComponentModels
+{
+    public class Carousel
+    {
+        public int ActiveIndex { get; private set; }
+        public int NumPages = -1;
+        Action? OnUpdateIndex;
+
+        public Carousel(int pages,Action onUpdateIndex) {
+            ActiveIndex = 0;
+            NumPages = pages;
+            OnUpdateIndex = onUpdateIndex;
+        }
+
+        public void SetCaroselIndex(int newIndex)
+        {
+            if (NumPages <= 0)
+            {
+                return;
+            }
+            if (newIndex >= NumPages)
+            {
+                newIndex = NumPages - 1;
+            }
+            else if (newIndex < 0)
+            {
+                newIndex = 0;
+            }
+
+            ActiveIndex = newIndex;
+            OnUpdateIndex?.Invoke();
+        }
+    }
+}
