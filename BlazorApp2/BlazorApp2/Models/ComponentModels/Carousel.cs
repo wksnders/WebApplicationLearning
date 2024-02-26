01c43cf1ec762bd3bfe1a92777f27d3b3e75e9c4
wksnders <42388072+wksnders@users.noreply.github.com>
2024-02-25 21:53:30 -0700

    updated dot selector

--- BlazorApp2/BlazorApp2/Models/ComponentModels/Carousel.cs
+++ BlazorApp2/BlazorApp2/Models/ComponentModels/Carousel.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Reflection;
 
 namespace BlazorApp2.Models.ComponentModels
 {
@@ -7,12 +8,23 @@ namespace BlazorApp2.Models.ComponentModels
         public int ActiveIndex { get; private set; }
         public int NumPages = -1;
         Action? OnUpdateIndex;
+        //bool isautoAdvancingCarosel = true;
 
         public Carousel(int pages,Action onUpdateIndex) {
             ActiveIndex = 0;
             NumPages = pages;
             OnUpdateIndex = onUpdateIndex;
         }
+        /*
+        public async Task AutoAdvanceCarosel()
+        {
+            await Task.Delay(2500);
+            while (isautoAdvancingCarosel)
+            {
+                SetCaroselIndex(ActiveIndex + 1);
+                await Task.Delay(2500);
+            }
+        }*/
 
         public void SetCaroselIndex(int newIndex)
         {
